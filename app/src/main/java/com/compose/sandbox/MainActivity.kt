b53cd1efa8ab9f2bc1c0aaa3d504e8d2da199cdb
evgeniy.sibagatullin <evgeniy.sibagatullin@gmail.com>
2021-05-26 22:30:17 +0200

    Add next move cell type text indicator.

--- app/src/main/java/com/compose/sandbox/MainActivity.kt
+++ app/src/main/java/com/compose/sandbox/MainActivity.kt
@@ -8,17 +8,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.compose.sandbox.MainActivity.Companion.COLUMNS
 import com.compose.sandbox.MainActivity.Companion.ROWS
 import com.compose.sandbox.ui.theme.SandboxTheme
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -65,6 +69,20 @@ private fun MyScreenContent() {
     }
 
     Column(verticalArrangement = Arrangement.Bottom) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "${newCellType.name.toLowerCase(Locale.getDefault())} move",
+                color = Color.Yellow,
+                fontSize = 30.sp
+            )
+        }
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
